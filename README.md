aa8f4271f1361260ba92ec2cbeef7f31b88ebc55
Moshekwa <malatjim04@gmail.com>
2024-11-18 02:57:53 +0200

    imepleted database models, configured database and run migrations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Django-rest-framework
+
+# Create App Syntax, Run Migrations\
+# DockerFile Or Docker-Compose
--- firstproject/firstApp/views.py
+++ firstproject/firstApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from firstApp.models import Employee
 
 # Create your views here.
 def employeeView(request):
@@ -8,5 +9,8 @@ def employeeView(request):
         'name':'John',
         'salary':50000
     }
-    
-    return JsonResponse(emp)
+
+    data = Employee.objects.all();
+    response = {'employees': list(data.values('name','salary'))}
+
+    return JsonResponse(response)
